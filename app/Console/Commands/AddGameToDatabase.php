36a2b07f6ced3e067284e3aaca246a88fab9bd49
vehikl-laumei <laumei@vshred.com>
2024-06-14 09:33:37 -0400

    Command allows for user to input a game name and add it to the 'games' database.

--- app/Console/Commands/AddGameToDatabase.php
+++ app/Console/Commands/AddGameToDatabase.php
@@ -2,9 +2,11 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Game;
 use GuzzleHttp\Client;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
 
 class AddGameToDatabase extends Command
 {
@@ -13,24 +15,43 @@ class AddGameToDatabase extends Command
      *
      * @var string
      */
-    protected $signature = 'app:add-game-to-database';
+    protected $signature = 'app:add-game-to-database {gameName}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Add a game to the database from the API';
 
     /**
      * Execute the console command.
      */
-    public function handle(String $gameName)
+    public function handle()
     {
-        $apiKey = Config::get('retro-spirit.key');
-        $baseURL = Config::get('retro-spirit.url');
+        try {
+            $gameName = $this->argument('gameName');
+            $apiKey = Config::get('retro-spirit.key');
+            $baseURL = Config::get('retro-spirit.url');
 
-        $client = new Client();
-        $response = $client->get("$baseURL/api/search/games/$gameName?key=$apiKey");
+            $client = new Client();
+            $response = $client->get("$baseURL/api/search/games/$gameName?key=$apiKey");
+
+            $body = $response->getBody();
+            $contents = $body->getContents();
+            $data = json_decode($contents, true);
+
+            if (isset($data["results"]["0"])) {
+                $game = new Game();
+                $game->name = $data["results"]["0"]["name"];
+                $game->year = $data["results"]["0"]["year"];
+                $game->description = $data["results"]["0"]["description"];
+                $game->save();
+            } else {
+                $this->info('No results found for the given game name.');
+            }
+        } catch (\Exception $e) {
+            $this->error('An error occurred while adding the game to the database: ' . $e->getMessage());
+        }
     }
 }
--- app/Models/Game.php
+++ app/Models/Game.php
@@ -9,5 +9,5 @@ class Game extends Model
 {
     use HasFactory;
 
-
+    protected $fillable = ['name', 'year', 'description'];
 }
