d00b40c58a2dc34fc2112d964006984dbb350ad8
Caleb <caleb.brown9999@gmail.com>
2024-09-23 19:16:12 -0400

    Add genre seeder

--- database/seeders/GenreSeeder.php
+++ database/seeders/GenreSeeder.php
@@ -2,15 +2,12 @@
 
 namespace Database\Seeders;
 
-use App\Models\Game;
-use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
-class GameUserSeeder extends Seeder
+class GenreSeeder extends Seeder
 {
     public function run(): void
     {
-
+        exec('php artisan import:genres');
     }
 }
