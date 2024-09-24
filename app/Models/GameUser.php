65db502007f4dbee37c6305650c4aa5a82c78583
Caleb <caleb.brown9999@gmail.com>
2024-09-24 11:25:56 -0400

    Added GameUser Model

---
+++ app/Models/GameUser.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GameUser extends Model
+{
+    use HasFactory;
+
+    protected $table = 'game_user';
+}
